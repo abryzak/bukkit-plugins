fd7fcc54a6213c3499c88b36ac30b261b1296774
Adam Bryzak <adam@bryzak.net>
2011-12-19 21:33:28 +1000

    Added logging

--- bukkit-givemeop/src/net/bryzak/bukkit/HelloWorld.java
+++ bukkit-givemeop/src/net/bryzak/bukkit/HelloWorld.java
@@ -1,5 +1,17 @@
 package net.bryzak.bukkit;
 
-public class HelloWorld {
+import java.util.logging.Logger;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HelloWorld extends JavaPlugin{
+    Logger log = Logger.getLogger("Minecraft");
+    
+    public void onEnable(){
+        log.info("Hello World!");
+    }
+    
+    public void onDisable(){
+        log.info("So Long World!");
+    }
 }
